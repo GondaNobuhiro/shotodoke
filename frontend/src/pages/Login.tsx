c9653d828b9f79960a9cb3c2559c1bd8033cc0bd
NOBBY for Mac <nobby4062@gmail.com>
2024-02-17 11:23:58 +0900

    不要画面削除

--- frontend/src/pages/Login.tsx
+++
@@ -1,8 +0,0 @@
-import {useNavigate} from "react-router-dom";
-import {useEffect} from "react";
-export function Login() {
-    const navigate = useNavigate();
-    useEffect(() => {
-        
-    });
-}
